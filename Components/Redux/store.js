d6749fe9a46473994c52c5f626c6f5aacd9dcb08
Jay Pithava <js106085@gmail.com>
2024-04-02 12:30:25 +0530

    Saga added redux

--- Components/Redux/store.js
+++ Components/Redux/store.js
@@ -1,8 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import runSaga from 'redux-saga';
+import SagaData from './saga';
+
+const sagaMiddleware = runSaga();
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: () => [sagaMiddleware],
 });
 
+sagaMiddleware.run(SagaData);
 export default store;
